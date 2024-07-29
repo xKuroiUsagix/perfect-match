a6e1a24d4d7756ed04afe60e3a8661499993d6d4
xKuroiUsagix <povoroznikvitalii@gmail.com>
2024-07-29 12:10:54 +0300

    move max field length to constants

--- bot/db/models.py
+++ bot/db/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.sql.functions import current_timestamp
 
-from .constants import OTHER, STATE_START
+from .constants import OTHER, STATE_START, MAX_NAME_LENGTH, MAX_CITY_LENGTH, MAX_DESCRIPTION_LENGTH
 
 
 class Base(DeclarativeBase):
@@ -19,12 +19,12 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     telegram_id: Mapped[int] = mapped_column(Integer(), unique=True, index=True)
     chat_id: Mapped[int] = mapped_column(Integer(), unique=True)
-    name: Mapped[Optional[str]] = mapped_column(String(64))
+    name: Mapped[Optional[str]] = mapped_column(String(MAX_NAME_LENGTH))
     age: Mapped[int] = mapped_column(Integer(), default=0, index=True)
-    city: Mapped[Optional[str]] = mapped_column(String(64), index=True)
+    city: Mapped[Optional[str]] = mapped_column(String(MAX_CITY_LENGTH), index=True)
     gender: Mapped[int] = mapped_column(Integer(), default=OTHER)
     looking_for: Mapped[Optional[int]] = mapped_column(Integer())
-    description: Mapped[Optional[str]] = mapped_column(String(512))
+    description: Mapped[Optional[str]] = mapped_column(String(MAX_DESCRIPTION_LENGTH))
     created_at: Mapped[DateTime] = mapped_column(DateTime(), server_default=current_timestamp())
 
     def __repr__(self) -> str:
